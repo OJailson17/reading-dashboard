dda307f627c7eee2442187f1c52c738484096cd3
OJailson17 <JaylsonO17@gmail.com>
2023-03-07 22:53:45 -0300

    chore: redirect user to dashboard page if auth token already exists

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,17 @@
+import { cookies } from 'next/headers';
 import { PageTitle } from '@/styles/common';
 import { FormComponent } from './components/Form';
+import { redirect } from 'next/navigation';
 
 export default function Login() {
+	// Get token from cookies
+	const token = cookies().has('@reading_dashboard:token');
+
+	// Redirect to login page if token already exists
+	if (token) {
+		redirect('/');
+	}
+
 	return (
 		<div>
 			<PageTitle>Login</PageTitle>
