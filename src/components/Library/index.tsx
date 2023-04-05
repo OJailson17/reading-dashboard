79bec2e8a97d19f5fd281099197f1c74f235b263
OJailson17 <JaylsonO17@gmail.com>
2023-04-04 22:18:50 -0300

    fix: book types

--- src/components/Library/index.tsx
+++ src/components/Library/index.tsx
@@ -1,66 +1,8 @@
 'use client';
 
-import * as Dialog from '@radix-ui/react-dialog';
-import { useState, SyntheticEvent } from 'react';
-import { BookDialog } from '../BookDialog';
+import { Book } from '@/types/bookTypes';
 import { BookSlideComponent } from './BookSlide';
-import {
-	BookComponent,
-	BookSlide,
-	LibraryComponent,
-	LibraryComponentWrapper,
-} from './styles';
-
-interface TitleProperty {
-	plain_text: string;
-}
-
-interface Book {
-	object: string;
-	id: string;
-	icon: {
-		external: {
-			url: string;
-		};
-	};
-	properties: {
-		Author: {
-			id: string;
-			rich_text: TitleProperty[];
-		};
-		Rating: {
-			id: string;
-			type: string;
-			select: {
-				name: string;
-			};
-		};
-		Status: {
-			id: string;
-			type: string;
-			select: {
-				id: string;
-				name: string;
-				color: string;
-			};
-		};
-		Name: {
-			id: string;
-			type: string;
-			title: TitleProperty[];
-		};
-		'Current Page': {
-			id: string;
-			type: number;
-			number: number;
-		};
-		'Qtd. Pages': {
-			id: string;
-			type: number;
-			number: number;
-		};
-	};
-}
+import { BookSlide, LibraryComponent, LibraryComponentWrapper } from './styles';
 
 interface LibraryBooks {
 	reading_books: Book[];
