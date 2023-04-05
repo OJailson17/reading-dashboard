8ed6776ec0ae8bac66c395887b238b806dba9214
OJailson17 <JaylsonO17@gmail.com>
2023-04-05 20:08:53 -0300

    chore: check if user is logged before update page value

--- src/components/BookDialog/index.tsx
+++ src/components/BookDialog/index.tsx
@@ -5,9 +5,11 @@ import { useBook } from '@/context/BookContext';
 import { api } from '@/lib/axios';
 import { Book } from '@/types/bookTypes';
 import * as Dialog from '@radix-ui/react-dialog';
+import { useRouter } from 'next/navigation';
 import { parseCookies } from 'nookies';
 import { useEffect, useState } from 'react';
 import { Rings } from 'react-loading-icons';
+import { toast, ToastContainer } from 'react-toastify';
 
 import {
 	DialogClose,
@@ -18,6 +20,8 @@ import {
 	DialogTitle,
 } from './styles';
 
+import 'react-toastify/dist/ReactToastify.css';
+
 interface BookDialogProps {
 	book: Book | null;
 }
@@ -27,6 +31,8 @@ export const BookDialog = ({ book }: BookDialogProps) => {
 	const [showSaveButton, setShowSaveButton] = useState(false);
 	const [isPageInputDisable, setIsPageInputDisable] = useState(true);
 
+	const router = useRouter();
+
 	// Get token from cookies
 	const { '@reading_dashboard:token': token } = parseCookies();
 	// Get database id from cookies
@@ -35,11 +41,29 @@ export const BookDialog = ({ book }: BookDialogProps) => {
 	// Books hook
 	const { onGetBooks } = useBook();
 
+	// Update current page value
 	const updatePages = async () => {
 		// Disable input
 		setIsPageInputDisable(true);
 
+		// Redirect user to login page if token or database id does not exist
+		if (!token || !databaseId) {
+			toast('Error', {
+				position: 'top-center',
+				autoClose: 2000,
+				theme: 'dark',
+				type: 'error',
+			});
+
+			setTimeout(() => {
+				router.push('/login');
+			}, 2500);
+
+			return;
+		}
+
 		try {
+			// Make the api call to passing the new value
 			const response = await api.patch('/book/update', {
 				current_page: currentPage,
 				page_id: book?.id,
@@ -82,89 +106,92 @@ export const BookDialog = ({ book }: BookDialogProps) => {
 	}, [book]);
 
 	return (
-		<Dialog.Portal>
-			<DialogOverlay />
+		<>
+			<ToastContainer />
+			<Dialog.Portal>
+				<DialogOverlay />
 
-			<DialogContent>
-				<DialogTitle>{book?.properties.Name.title[0].plain_text}</DialogTitle>
-				<DialogContentContainer>
-					{/* If there is a book cover, show the image, if not, show the placeholder cover */}
-					{book?.icon?.external?.url ? (
-						<img src={book?.icon.external.url} alt='' />
-					) : (
-						<div
-							className='placeholder-cover-dialog'
-							data-title={book?.properties.Name.title[0].plain_text}
-						>
-							<p>{book?.properties.Name.title[0].plain_text}</p>
-						</div>
-					)}
-					<DialogContentBookInfo>
-						<div>
-							<span>Author:</span>
-							<span>{book?.properties.Author.rich_text[0].plain_text}</span>
-						</div>
-						<div>
-							<span>Status:</span>
-							<span className={book?.properties.Status.select.name}>
-								{book?.properties.Status.select.name}
-							</span>
-						</div>
-						<div>
-							<span>Pages:</span>
-							<span>{book?.properties['Qtd. Pages'].number}</span>
-						</div>
-						{/* Show the rating ig the book status is Finished */}
-						{book?.properties.Status.select.name === 'Finished' ? (
-							<div>
-								<span>Rating</span>
-								<span>{book?.properties.Rating.select.name}</span>
-							</div>
+				<DialogContent>
+					<DialogTitle>{book?.properties.Name.title[0].plain_text}</DialogTitle>
+					<DialogContentContainer>
+						{/* If there is a book cover, show the image, if not, show the placeholder cover */}
+						{book?.icon?.external?.url ? (
+							<img src={book?.icon.external.url} alt='' />
 						) : (
-							<div>
-								<span>Current Page:</span>
-
-								{/* If the state value is less than 0, show default value */}
-								<input
-									type='number'
-									value={
-										currentPage < 0
-											? book?.properties['Current Page'].number
-											: currentPage
-									}
-									onChange={e => setCurrentPage(Number(e.target.value))}
-									disabled={isPageInputDisable}
-								/>
-
-								{/* Show save or edit button depending on the showSaveButton state */}
-								{showSaveButton ? (
-									<button
-										className='book-btn'
-										onClick={updatePages}
-										disabled={isPageInputDisable}
-									>
-										{isPageInputDisable ? (
-											<Rings width={16} height={16} />
-										) : (
-											<p>Save</p>
-										)}
-									</button>
-								) : (
-									<button
-										className='book-btn'
-										onClick={() => {
-											setIsPageInputDisable(false);
-										}}
-									>
-										Edit
-									</button>
-								)}
+							<div
+								className='placeholder-cover-dialog'
+								data-title={book?.properties.Name.title[0].plain_text}
+							>
+								<p>{book?.properties.Name.title[0].plain_text}</p>
 							</div>
 						)}
-					</DialogContentBookInfo>
-				</DialogContentContainer>
-				<DialogClose onClick={onCloseModal}>Close</DialogClose>
-			</DialogContent>
-		</Dialog.Portal>
+						<DialogContentBookInfo>
+							<div>
+								<span>Author:</span>
+								<span>{book?.properties.Author.rich_text[0].plain_text}</span>
+							</div>
+							<div>
+								<span>Status:</span>
+								<span className={book?.properties.Status.select.name}>
+									{book?.properties.Status.select.name}
+								</span>
+							</div>
+							<div>
+								<span>Pages:</span>
+								<span>{book?.properties['Qtd. Pages'].number}</span>
+							</div>
+							{/* Show the rating ig the book status is Finished */}
+							{book?.properties.Status.select.name === 'Finished' ? (
+								<div>
+									<span>Rating</span>
+									<span>{book?.properties.Rating.select.name}</span>
+								</div>
+							) : (
+								<div>
+									<span>Current Page:</span>
+
+									{/* If the state value is less than 0, show default value */}
+									<input
+										type='number'
+										value={
+											currentPage < 0
+												? book?.properties['Current Page'].number
+												: currentPage
+										}
+										onChange={e => setCurrentPage(Number(e.target.value))}
+										disabled={isPageInputDisable}
+									/>
+
+									{/* Show save or edit button depending on the showSaveButton state */}
+									{showSaveButton ? (
+										<button
+											className='book-btn'
+											onClick={updatePages}
+											disabled={isPageInputDisable}
+										>
+											{isPageInputDisable ? (
+												<Rings width={16} height={16} />
+											) : (
+												<p>Save</p>
+											)}
+										</button>
+									) : (
+										<button
+											className='book-btn'
+											onClick={() => {
+												setIsPageInputDisable(false);
+											}}
+										>
+											Edit
+										</button>
+									)}
+								</div>
+							)}
+						</DialogContentBookInfo>
+					</DialogContentContainer>
+					<DialogClose onClick={onCloseModal}>Close</DialogClose>
+				</DialogContent>
+			</Dialog.Portal>
+		</>
 	);
 };
