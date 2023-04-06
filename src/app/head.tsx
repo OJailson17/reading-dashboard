63bec38ccd99f9c959de7ae307dcef143c1cab7e
OJailson17 <JaylsonO17@gmail.com>
2023-04-06 20:49:50 -0300

    chore: remove android icons

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,44 +1,6 @@
 export default function Head() {
 	return (
 		<>
-			<link
-				rel='icon'
-				type='image/png'
-				sizes='192x192'
-				href='/public/android-icon-192x192.png'
-			/>
-			<link
-				rel='icon'
-				type='image/png'
-				sizes='192x192'
-				href='/public/android-icon-192x192.png'
-			/>
-			<link
-				rel='icon'
-				type='image/png'
-				sizes='192x192'
-				href='/public/android-icon-192x192.png'
-			/>
-			<link
-				rel='icon'
-				type='image/png'
-				sizes='192x192'
-				href='/public/android-icon-192x192.png'
-			/>
-			<link
-				rel='icon'
-				type='image/png'
-				sizes='192x192'
-				href='/public/android-icon-192x192.png'
-			/>
-			<link
-				rel='icon'
-				type='image/png'
-				sizes='192x192'
-				href='/public/android-icon-192x192.png'
-			/>
-			<meta name='theme-color' content='#ffffff'></meta>
-
 			<link rel='shortcut icon' href='/favicon.ico' type='image/x-icon' />
 
 			<title>Reading Dashboard</title>
