4629a4511f4d81e8956381ca54bee909549e29bc
OJailson17 <JaylsonO17@gmail.com>
2023-06-16 00:01:20 -0300

    chore: remove dynamic api url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,14 +1,6 @@
 import axios from 'axios';
 
-let baseURL = process.env.API_BASE_URL;
-
-if (typeof window !== 'undefined') {
-	console.log(window.location.href);
-
-	if (process.env.NODE_ENV !== 'production') {
-		baseURL = `${window.location.href}/api`;
-	}
-}
+const baseURL = process.env.API_BASE_URL;
 
 export const api = axios.create({
 	baseURL,
