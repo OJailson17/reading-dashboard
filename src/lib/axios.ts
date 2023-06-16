95b8071d7bedb24c0b5d14dc57b3815790553cc9
OJailson17 <JaylsonO17@gmail.com>
2023-06-15 23:20:00 -0300

    chore: use dynamic api url when is not in prod

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,15 @@
 import axios from 'axios';
 
-const baseUrl = process.env.API_BASE_URL;
+let baseURL = process.env.API_BASE_URL;
+
+if (typeof window !== 'undefined') {
+	console.log(window.location.href);
+
+	if (process.env.NODE_ENV !== 'production') {
+		baseURL = `${window.location.href}/api`;
+	}
+}
 
 export const api = axios.create({
-	baseURL: baseUrl,
+	baseURL,
 });
