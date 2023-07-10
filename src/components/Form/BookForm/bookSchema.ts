192d5b62ffa05869fe14e94ce2d07a19ac654115
OJailson17 <JaylsonO17@gmail.com>
2023-07-10 20:54:42 -0300

    chore: custom input error messages

--- src/components/Form/BookForm/bookSchema.ts
+++ src/components/Form/BookForm/bookSchema.ts
@@ -1,21 +1,43 @@
 import * as yup from 'yup';
 
 export const bookSchema = yup.object({
-	name: yup.string().trim().required(),
+	name: yup.string().trim().required('name is required'),
 	icon_url: yup.string().trim().optional(),
-	genres: yup.array().ensure().of(yup.string().required()).required(),
-	author: yup.string().trim().required(),
-	status: yup.string().oneOf(['To read', 'Reading', 'Finished']).required(),
-	language: yup.string().oneOf(['Portuguese', 'English']).required(),
-	qtd_page: yup.number().min(1).required(),
-	current_page: yup.number().default(0).required(),
-	goodreads_review: yup.string().trim().default('none').required(),
+	genres: yup
+		.array()
+		.ensure()
+		.of(yup.string().required())
+		.required('genre is required'),
+	author: yup.string().trim().required('author is required'),
+	status: yup
+		.string()
+		.oneOf(['To read', 'Reading', 'Finished'])
+		.required('status is required'),
+	language: yup
+		.string()
+		.oneOf(['Portuguese', 'English'])
+		.required('language is required'),
+	qtd_page: yup
+		.number()
+		.min(1, 'it must be grater than or equal to 1')
+		.required('page quantity is required')
+		.typeError('pages quantity needs to be a number'),
+	current_page: yup
+		.number()
+		.default(0)
+		.required()
+		.typeError('current page needs to be a number'),
+	goodreads_review: yup
+		.string()
+		.trim()
+		.default('none')
+		.required('goodreads review is required'),
 	started_date: yup
 		.string()
 		.trim()
 		.when('status', {
 			is: (value: string) => value === 'Reading' || value === 'Finished',
-			then: schema => schema.required(),
+			then: schema => schema.required('started date is required'),
 			otherwise: schema => schema.nullable(),
 		}),
 	finished_date: yup
@@ -23,7 +45,8 @@ export const bookSchema = yup.object({
 		.trim()
 		.when('status', {
 			is: 'Finished',
-			then: schema => schema.required(),
+			then: schema =>
+				schema.required('started and finished dates are required'),
 			otherwise: schema => schema.nullable(),
 		}),
 	book_review: yup.string().trim().optional().default('none'),
