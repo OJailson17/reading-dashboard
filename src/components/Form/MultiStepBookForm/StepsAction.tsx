2fbd00a8fba1677585231df13f0bf3e5bd492d15
OJailson17 <JaylsonO17@gmail.com>
2023-10-05 23:54:52 -0300

    chore: add loading icon on create book button

--- src/components/Form/MultiStepBookForm/StepsAction.tsx
+++ src/components/Form/MultiStepBookForm/StepsAction.tsx
@@ -2,6 +2,7 @@ import React, { ButtonHTMLAttributes } from 'react';
 import { CreateBook } from '../BookForm';
 import { UseFormHandleSubmit } from 'react-hook-form';
 import { MultiStepActionButton, MultiStepActionContainer } from './styles';
+import { Puff } from 'react-loading-icons';
 
 interface FormStepsActionProps {
 	step: number;
@@ -24,7 +25,15 @@ export const FormStepsAction = ({
 				</MultiStepActionButton>
 			)}
 			<MultiStepActionButton type='button' onClick={onHandleSubmit}>
-				{!isLoading ? (step >= 8 ? 'Create' : 'Next') : 'loading'}
+				{!isLoading ? (
+					step >= 8 ? (
+						'Create'
+					) : (
+						'Next'
+					)
+				) : (
+					<Puff width={20} height={20} stroke='#32ccbc' />
+				)}
 			</MultiStepActionButton>
 		</MultiStepActionContainer>
 	);
