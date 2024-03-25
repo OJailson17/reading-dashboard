11cfab2a51e12a40828974aedf132349235bd55b
OJailson17 <JaylsonO17@gmail.com>
2024-03-25 17:43:28 -0300

    chore:add security to revalidate route

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,7 +1,28 @@
 import { revalidateTag } from 'next/cache';
 import { NextRequest, NextResponse } from 'next/server';
+import bcryptjs from 'bcryptjs';
 
-export async function GET(req: NextRequest, res: NextResponse) {
+export async function POST(req: NextRequest, res: NextResponse) {
+	const { password } = (await req.json()) as {
+		password: string;
+	};
+
+	const doesPasswordMatch = await bcryptjs.compare(
+		password,
+		`${process.env.API_REVALIDATE_PASSWORD_HASH}`,
+	);
+
+	if (!doesPasswordMatch) {
+		return NextResponse.json(
+			{
+				error: 'Not allowed',
+			},
+			{
+				status: 401,
+			},
+		);
+	}
+
 	revalidateTag('fetch-books');
 	revalidateTag('sign-in');
 
