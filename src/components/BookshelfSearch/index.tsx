91cd6ef09dc8820a3f350bdfbaa04e1ae71cec2c
OJailson17 <JaylsonO17@gmail.com>
2024-04-15 02:17:00 -0300

    chore: add a reset search button

--- src/components/BookshelfSearch/index.tsx
+++ src/components/BookshelfSearch/index.tsx
@@ -1,12 +1,11 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { IoIosSearch } from 'react-icons/io';
+import { IoCloseOutline } from 'react-icons/io5';
 import * as yup from 'yup';
 
-import { Tab } from '@/app/bookshelf/page';
 import { yupResolver } from '@hookform/resolvers/yup';
 
 const searchFormSchema = yup.object({
@@ -16,17 +15,19 @@ const searchFormSchema = yup.object({
 type SearchBookForm = yup.InferType<typeof searchFormSchema>;
 
 export const BookshelfSearch = () => {
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-	} = useForm<SearchBookForm>({
-		resolver: yupResolver(searchFormSchema),
-	});
+	const { register, handleSubmit, resetField, watch } = useForm<SearchBookForm>(
+		{
+			resolver: yupResolver(searchFormSchema),
+		},
+	);
 
+	const queryFieldWatch = watch('query');
 	const router = useRouter();
 
 	const handleSearch = ({ query }: SearchBookForm) => {
+		// const activeElement = document.activeElement as HTMLInputElement;
+		// activeElement.blur();
+
 		if (!query || query.trim() === '') {
 			return router.push(`/bookshelf/?tab=all`, {
 				scroll: false,
@@ -38,20 +39,40 @@ export const BookshelfSearch = () => {
 		});
 	};
 
+	const handleResetSearch = () => {
+		resetField('query');
+
+		router.push(`/bookshelf/?tab=all`, {
+			scroll: false,
+		});
+	};
+
 	return (
 		<form
 			onSubmit={handleSubmit(handleSearch)}
 			autoComplete='off'
-			className='w-full xs:max-[500px]:h-12 sm:w-80 h-10 bg-background flex items-center justify-center gap-3 pl-5 rounded-full'
+			className='w-full xs:max-[500px]:h-12 sm:w-80 h-10 bg-background flex items-center justify-center gap-3 shrink-0 px-5 rounded-full'
 		>
 			<IoIosSearch size={25} />
 
 			<input
 				type='text'
 				placeholder='Search books'
-				className='w-full h-full bg-transparent text-span rounded-tr-full rounded-br-full px-3 placeholder:text-placeholder placeholder:text-sm outline-none'
+				className='w-full max-w-72 h-full bg-transparent text-span px-3 placeholder:text-placeholder placeholder:text-sm outline-none'
 				{...register('query')}
 			/>
+
+			{queryFieldWatch && queryFieldWatch.length > 0 ? (
+				<button
+					onClick={handleResetSearch}
+					className='bg-transparent border-none rounded-sm'
+					type='button'
+				>
+					<IoCloseOutline size={20} />
+				</button>
+			) : (
+				<div className='w-5 h-5 bg-transparent' />
+			)}
 		</form>
 	);
 };
