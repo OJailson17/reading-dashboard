d994bc4c7affbb4cd8513226c688d79f6ac23114
OJailson17 <JaylsonO17@gmail.com>
2024-05-13 23:13:37 -0300

    test: setup tests

---
+++ vitest.config.mjs
@@ -0,0 +1,14 @@
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    environment: 'jsdom',
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+    },
+  },
+});
