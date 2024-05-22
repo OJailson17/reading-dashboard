71787567ec88c52a085b72ab64dc6911bc8da0f3
OJailson17 <JaylsonO17@gmail.com>
2024-05-22 10:30:17 -0300

    chore: sort reading books by progress

--- src/components/ReadingCard/index.tsx
+++ src/components/ReadingCard/index.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Link from 'next/link';
 
 import { Book } from '@/@types/book';
@@ -11,8 +13,27 @@ interface ReadingCardProps {
   books: Book[];
 }
 
+interface SortByProgressProps {
+  readingBooks: Book[];
+}
+
+const calculatePercentage = (total: number, current: number) => {
+  return Math.round((current / total) * 100);
+};
+
+// Get a list of books and sort by progress Desc
+const sortByProgress = ({ readingBooks }: SortByProgressProps) => {
+  return readingBooks.slice().sort((a, b) => {
+    const progressA = calculatePercentage(a.total_pages, a.current_page);
+    const progressB = calculatePercentage(b.total_pages, b.current_page);
+    return progressB - progressA;
+  });
+};
+
 export const ReadingCard = async ({ books }: ReadingCardProps) => {
   const readingBooks = books.filter((book) => book.status === 'Reading');
+  // sort book by progress Desc
+  const booksByProgress = sortByProgress({ readingBooks });
 
   return (
     <div className="h-96 w-full max-w-[403px] rounded-2xl bg-secondary-background py-6 xs:px-4 sm:px-7">
@@ -27,7 +48,7 @@ export const ReadingCard = async ({ books }: ReadingCardProps) => {
       </header>
 
       <div className="books-container mt-9 h-60 space-y-6 overflow-y-auto py-1 pl-1 pr-3">
-        {readingBooks.map((book, i) => (
+        {booksByProgress.map((book, i) => (
           <Drawer key={book.id}>
             <ReadingBookStats book={book} />
             <UpdateReadingDialog book={book} />
