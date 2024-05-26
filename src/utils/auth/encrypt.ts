3010b948f3835c469ece0458f117b6ff68400098
OJailson17 <JaylsonO17@gmail.com>
2024-05-26 20:05:36 -0300

    chore: increase session cookie exipiration date

--- src/utils/auth/encrypt.ts
+++ src/utils/auth/encrypt.ts
@@ -4,13 +4,13 @@ const secretKey = process.env.ENCRYPT_SECRET_KEY;
 const key = new TextEncoder().encode(secretKey);
 
 type Payload = {
-	database_id: string;
+  database_id: string;
 };
 
 export async function encrypt(payload: Payload) {
-	return await new SignJWT(payload)
-		.setProtectedHeader({ alg: 'HS256' })
-		.setIssuedAt()
-		.setExpirationTime('1 day')
-		.sign(key);
+  return await new SignJWT(payload)
+    .setProtectedHeader({ alg: 'HS256' })
+    .setIssuedAt()
+    .setExpirationTime('7 days')
+    .sign(key);
 }
