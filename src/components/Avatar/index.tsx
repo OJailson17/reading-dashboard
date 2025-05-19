30ec3cf9cab4aadaa45a777bf08fcc86df523051
OJailson17 <JaylsonO17@gmail.com>
2025-05-18 22:17:28 -0300

    fix: avatar hydration error due to differences between client and server rendering

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,17 +1,27 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import Avatar, { genConfig } from 'react-nice-avatar';
 
-const config = genConfig({
-  sex: 'man',
-  earSize: 'small',
-  hairColor: 'black',
-  hairStyle: 'thick',
-  bgColor: '#5CDAC3',
-  hatStyle: 'none',
-  shirtStyle: 'polo',
-});
-
 export const MyAvatar = () => {
+  const [config, setConfig] = useState<any>(null);
+
+  useEffect(() => {
+    setConfig(
+      genConfig({
+        sex: 'man',
+        earSize: 'small',
+        hairColor: 'black',
+        hairStyle: 'thick',
+        bgColor: '#5CDAC3',
+        hatStyle: 'none',
+        shirtStyle: 'polo',
+      }),
+    );
+  }, []);
+
+  if (!config)
+    return <div className="h-full w-full rounded-full bg-[#5CDAC3]" />;
+
   return <Avatar className="h-full w-full" {...config} />;
 };
